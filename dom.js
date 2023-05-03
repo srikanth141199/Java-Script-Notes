f8a333d9d676a9154f4766078ccd554f18812d58
srikanth141199 <60726437+srikanth141199@users.noreply.github.com>
2023-05-03 20:53:31 +0530

    2 files modified

--- dom.js
+++ dom.js
@@ -1,12 +1,18 @@
 var counter = document.querySelector('.count');
+var data = document.querySelector('.text');
 
-console.log('inside dom.js');
+console.log('inside dom.js'); 
 
-var value = 1;
+var value = 0;
 
 setInterval(() => {
-  while (value < 10) {
+  while (value <= 10) {
+    console.log('value is  : ', value);
     value++;
     counter.innerHTML = value;
   }
 }, 1000);
+
+// setTimeout(() => {
+//   data.innerHTML = 'Count Updated';
+// }, 10000);
