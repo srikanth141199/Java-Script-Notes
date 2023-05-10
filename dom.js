0cdfa89fe82aaecd28c215ec68a59f7b297f90d6
srikanth141199 <60726437+srikanth141199@users.noreply.github.com>
2023-05-10 21:32:25 +0530

    5 files modified

--- dom.js
+++ dom.js
@@ -1,19 +1,16 @@
 var counter = document.querySelector('.count');
 var data = document.querySelector('.text');
 
-console.log('inside dom.js'); 
+console.log('inside dom.js');
 
 var value = 0;
 
 setInterval(() => {
-  while (value <= 10) {
+  if (value <= 10) {
     console.log('value is  : ', value);
     value++;
     counter.innerHTML = value;
   }
 }, 1000);
 
-// setTimeout(() => {
-//   data.innerHTML = 'Count Updated';
-// }, 10000);
- 
+
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import stylesheets
 import './style.css';
+import './dom.css';
 
 import './dom.js';
 
@@ -8,3 +9,17 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 //DOM :  Document Object Model
+
+var User = (fName, count) => {
+  (this.fName = fName),
+    (this.count = count),
+    (this.getCourseCount = () => {
+      console.log('Course count  is : ${this.count}');
+    });
+};
+
+// var Sri = User('Sri', 10);
+// console.log(Sri);
+
+var King = new User('King', 100);
+console.log(King);
